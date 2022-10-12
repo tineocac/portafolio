5666e7393e9727e495a0123395ad5fe0c3146d08
tineocac <carlostineocac@gmail.com>
2022-10-12 15:55:17 -0400

    portfolio section

--- js/main.js
+++ js/main.js
@@ -29,26 +29,50 @@ modalCloseBtns.forEach((modalCloseBtns) => {
   });
 });
 
-
-
 const downloadCvs = document.querySelectorAll(".download");
 
 const cvModals = document.querySelectorAll(".cv-modal");
 
 var modalCv = function (modalclick) {
-    cvModals[modalclick].classList.add("active");
-  };
-  
-  downloadCvs.forEach((downloadCvs, i) => {
-    downloadCvs.addEventListener("click", () => {
-      modalCv(i);
+  cvModals[modalclick].classList.add("active");
+};
+
+downloadCvs.forEach((downloadCvs, i) => {
+  downloadCvs.addEventListener("click", () => {
+    modalCv(i);
+  });
+});
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+  modalCloseBtns.addEventListener("click", () => {
+    cvModals.forEach((modalView) => {
+      modalView.classList.remove("active");
     });
   });
-  
-  modalCloseBtns.forEach((modalCloseBtns) => {
-    modalCloseBtns.addEventListener("click", () => {
-        cvModals.forEach((modalView) => {
-        modalView.classList.remove("active");
-      });
+});
+
+// portfolio modals
+
+const portfolioModel = document.querySelectorAll(".portfolio-model");
+
+const imgCard = document.querySelectorAll(".img-card");
+
+const portfolioCloseBtn = document.querySelectorAll(".portfolio-close-btn");
+
+var modalPortfolio = function (portfolioModalclick) {
+  portfolioModel[portfolioModalclick].classList.add("active");
+};
+
+imgCard.forEach((imgCard, i) => {
+  imgCard.addEventListener("click", () => {
+    modalPortfolio(i);
+  });
+});
+
+portfolioCloseBtn.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModel.forEach((portfolioModalView) => {
+      portfolioModalView.classList.remove("active");
     });
   });
+});
