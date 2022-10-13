f0e6fc11169b0fa2e7233dcf11948281ad3202e9
tineocac <carlostineocac@gmail.com>
2022-10-13 00:22:49 -0400

    dark theme

--- js/main.js
+++ js/main.js
@@ -76,3 +76,58 @@ portfolioCloseBtn.forEach((portfolioCloseBtn) => {
     });
   });
 });
+
+// website dark/light theme
+const themeBtn = document.querySelector(".theme-btn")
+
+themeBtn.addEventListener( "click", () => {
+  document.body.classList.toggle("dark-theme")
+  themeBtn.classList.toggle("sun")
+
+  localStorage.setItem("saved-theme", getCurrentTheme())
+  localStorage.setItem("saved-icon", getCurrentIcon())
+})
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light"
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon"
+
+const savedTheme = localStorage.getItem("saved-theme")
+const savedIcon = localStorage.getItem("saved-icon")
+
+if( savedTheme){
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"] ("dark-theme")
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"] ("sun")
+}
+
+// scrool to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn")
+
+window.addEventListener("scroll", function(){
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500)
+})
+
+scrollTopBtn.addEventListener("click", () =>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
+
+// navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section")
+  const scrollY = window.pageYOffset;
+
+  sections.forEach( current => {
+    let sectionHeight = current.offsetHeight
+    let sectionTop = current.offsetTop - 50
+    let id = current.getAttribute("id")
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
+      document.querySelector(".nav-items a[href*=" + id +"]").classList.add("active")
+    }
+    else{
+      document.querySelector(".nav-items a[href*=" + id +"]").classList.remove("active")
+    }
+
+  })
+})
+
