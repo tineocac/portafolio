40db2556999185075507a69ea7c178d54a9adcef
tineocac <carlostineocac@gmail.com>
2022-10-15 13:15:31 -0400

    portfolio correction & code extra deleted

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ const portfolioModel = document.querySelectorAll(".portfolio-model");
 
 const imgCard = document.querySelectorAll(".img-card");
 
-const portfolioCloseBtn = document.querySelectorAll(".portfolio-close-btn");
+const portfolioCloseBtn = document.querySelectorAll(".modal-close-btn");
 
 var modalPortfolio = function (portfolioModalclick) {
   portfolioModel[portfolioModalclick].classList.add("active");
@@ -78,107 +78,137 @@ portfolioCloseBtn.forEach((portfolioCloseBtn) => {
 });
 
 // website dark/light theme
-const themeBtn = document.querySelector(".theme-btn")
+const themeBtn = document.querySelector(".theme-btn");
 
-themeBtn.addEventListener( "click", () => {
-  document.body.classList.toggle("dark-theme")
-  themeBtn.classList.toggle("sun")
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
 
-  localStorage.setItem("saved-theme", getCurrentTheme())
-  localStorage.setItem("saved-icon", getCurrentIcon())
-})
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
+});
 
-const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light"
-const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon"
+const getCurrentTheme = () =>
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeBtn.classList.contains("sun") ? "sun" : "moon";
 
-const savedTheme = localStorage.getItem("saved-theme")
-const savedIcon = localStorage.getItem("saved-icon")
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
 
-if( savedTheme){
-  document.body.classList[savedTheme === "dark" ? "add" : "remove"] ("dark-theme")
-  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"] ("sun")
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
 }
 
 // scrool to top button
-const scrollTopBtn = document.querySelector(".scrollToTop-btn")
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
-  scrollTopBtn.classList.toggle("active", window.scrollY > 500)
-})
+window.addEventListener("scroll", function () {
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
 
-scrollTopBtn.addEventListener("click", () =>{
+scrollTopBtn.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-})
+});
 
 // navigation menu items active on page scroll
 window.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll("section")
+  const sections = document.querySelectorAll("section");
   const scrollY = window.pageYOffset;
 
-  sections.forEach( current => {
-    let sectionHeight = current.offsetHeight
-    let sectionTop = current.offsetTop - 50
-    let id = current.getAttribute("id")
+  sections.forEach((current) => {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let id = current.getAttribute("id");
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
-      document.querySelector(".nav-items a[href*=" + id +"]").classList.add("active")
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.remove("active");
     }
-    else{
-      document.querySelector(".nav-items a[href*=" + id +"]").classList.remove("active")
-    }
-
-  })
-})
+  });
+});
 
 //  responsive navigation menu toggle
 
 const btnMenu = document.querySelector(".nav-menu-btn");
 const navigation = document.querySelector(".navigation");
-const btnCloseMenu = document.querySelector(".nav-close-btn")
-const navItems = document.querySelectorAll(".nav-items a")
+const btnCloseMenu = document.querySelector(".nav-close-btn");
+const navItems = document.querySelectorAll(".nav-items a");
 
 btnMenu.addEventListener("click", () => {
-  navigation.classList.add("active")
-})
+  navigation.classList.add("active");
+});
 
 btnCloseMenu.addEventListener("click", () => {
-  navigation.classList.remove("active")
-})
+  navigation.classList.remove("active");
+});
 
 navItems.forEach((navItem) => {
   navItem.addEventListener("click", () => {
-    navigation.classList.remove("active")
-  })
-})
+    navigation.classList.remove("active");
+  });
+});
 
 // comomo styles scroll reveal
 
-ScrollReveal({ 
+ScrollReveal({
   // reset: true,
-  distance: '60px',
+  distance: "60px",
   duration: 2500,
-  delay: 100
+  delay: 100,
 });
 
-
 //  target elements, and specify options to create reveal animations
-ScrollReveal().reveal('.home .info h2, .section-title-1, .section-title-2', {delay: 500, origin: 'left'});
+ScrollReveal().reveal(".home .info h2, .section-title-1, .section-title-2", {
+  delay: 500,
+  origin: "left",
+});
 
-ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay: 600, origin: 'right'});
+ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn", {
+  delay: 600,
+  origin: "right",
+});
 
-ScrollReveal().reveal('.home .info .btn', {delay: 700, origin: 'bottom'});
+ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
 
-ScrollReveal().reveal('.media-icons i, .contact-left li', {delay: 500, origin: 'left', interval: 200});
+ScrollReveal().reveal(".media-icons i, .contact-left li", {
+  delay: 500,
+  origin: "left",
+  interval: 200,
+});
 
-ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom'});
+ScrollReveal().reveal(".about-img", { delay: 500, origin: "bottom" });
 
-ScrollReveal().reveal('.about .description, .contact-right', {delay: 600, origin: 'right'});
+ScrollReveal().reveal(".about .description, .contact-right", {
+  delay: 600,
+  origin: "right",
+});
 
-ScrollReveal().reveal('.about .professional-list li', {delay: 500, origin: 'right', interval: 200});
+ScrollReveal().reveal(".about .professional-list li", {
+  delay: 500,
+  origin: "right",
+  interval: 200,
+});
 
-ScrollReveal().reveal('.skills-description', {delay: 700, origin: 'left'});
+ScrollReveal().reveal(".skills-description", { delay: 700, origin: "left" });
 
-ScrollReveal().reveal('.experience-card, .education, .portfolio, .img-card',{delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal(".experience-card, .education, .img-card", {
+  delay: 800,
+  origin: "bottom",
+  interval: 200,
+});
 
-ScrollReveal().reveal('footer .group',{delay: 500, origin: 'top', interval: 200});
+ScrollReveal().reveal("footer .group", {
+  delay: 500,
+  origin: "top",
+  interval: 200,
+});
